a6e5b29039f12090ffde849c22f429e3209a19d5
Jahishigh <maxenceknoderer.mjm@gmail.com>
2017-04-03 16:57:13 +0200

    Bon header + color + logo

--- page-templates/front.php
+++ page-templates/front.php
@@ -10,17 +10,19 @@ get_header(); ?>
     <ul>
       <li class="head-top">
         <div class="title-top">
+          <img class="logo" src="wp-content/themes/themeportfolio/assets/images/logo.png" alt="Logo" />
           <h1><?php bloginfo( 'name' ); ?></h1>
           <h4><?php bloginfo( 'description' ); ?></h4>
-          <!-- <a href="#">Contact</a> -->
+          <a href="#" class="button contact">Contact</a>
+          <a href="#loop-start" class="button">Mes travaux</a>
         </div> <!-- .layer-->
-            <video autoplay loop class="fillWidth">
-              <source src="video/Fish-Tank.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+            <video autoplay loop class muted="fillWidth">
+              <source src="wp-content/themes/themeportfolio/assets/images/boat.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
             </video>
       </li> <!-- head-top -->
     </ul>
   </header> <!-- scroll-achor -->
-	<div class="scroll-anchor">
+	<div id="loop-start" class="scroll-anchor">
 		<?php do_action("MyPostAction") ?>
 	</div>
 </div> <!-- .projects-container -->
